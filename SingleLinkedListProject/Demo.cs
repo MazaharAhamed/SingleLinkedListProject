c8864ac0c4b76469248b75a9780fb50da46a7efb
MazaharAhamed <mazaharahamed77@gmail.com>
2020-07-30 21:54:04 +0530

    Add project files.

---
+++ SingleLinkedListProject/Demo.cs
@@ -0,0 +1,109 @@
+﻿using System;
+
+namespace SingleLinkedListProject
+{
+    class Demo
+    {
+        static void Main(string[] args)
+        {
+            int choice, data, k, x;
+
+            SingleLinkedList list = new SingleLinkedList();
+
+            list.CreateList();
+
+            while(true)
+            {
+                Console.WriteLine("1.Display List");
+                Console.WriteLine("2.Count the number of nodes");
+                Console.WriteLine("3.Search for an element");
+                Console.WriteLine("4.Insert in empty list/Insert in beginning of list");
+                Console.WriteLine("5.Insert a node at the end of the list");
+                Console.WriteLine("6.Insert a node after a specified node");
+                Console.WriteLine("7.Insert a node before a specified node");
+                Console.WriteLine("8.Insert a node at a given postion");
+                Console.WriteLine("9.Delete first node");
+                Console.WriteLine("10.Delete last node");
+                Console.WriteLine("11.Delete any node");
+                Console.WriteLine("12.Reverse the list");
+                Console.WriteLine("13.Bubble sort by exchanging data");
+                Console.WriteLine("14.Bubble sort by exchanging links");
+                Console.WriteLine("15.Quit");
+
+                Console.Write("Enter your choice:");
+                choice = Convert.ToInt32(Console.ReadLine());
+                
+                switch(choice)
+                {
+                    case 1:
+                        list.DisplayList();
+                        break;
+                    case 2:
+                        list.CountNodes();
+                        break;
+                    case 3:
+                        Console.Write("Enter the element to be searched:");
+                        data = Convert.ToInt32(Console.ReadLine());
+                        list.Search(data);
+                        break;
+                    case 4:
+                        Console.Write("Enter the element to be inserted:");
+                        data = Convert.ToInt32(Console.ReadLine());
+                        list.InsertInBeginning(data);
+                        break;
+                    case 5:
+                        Console.Write("Enter the element to be inserted:");
+                        data = Convert.ToInt32(Console.ReadLine());
+                        list.InsertInEnd(data);
+                        break;
+                    case 6:
+                        Console.Write("Enter the element to be inserted:");
+                        data = Convert.ToInt32(Console.ReadLine());
+                        Console.Write("Enter the element after which to insert:");
+                        x = Convert.ToInt32(Console.ReadLine());
+                        list.InsertANodeAfterGivenNode(data, x);
+                        break;
+                    case 7:
+                        Console.Write("Enter the element to be inserted:");
+                        data = Convert.ToInt32(Console.ReadLine());
+                        Console.Write("Enter the element before which to insert:");
+                        x = Convert.ToInt32(Console.ReadLine());
+                        list.InsertANodeBeforeGivenNode(data, x);
+                        break;
+                    case 8:
+                        Console.Write("Enter the element to be inserted:");
+                        data = Convert.ToInt32(Console.ReadLine());
+                        Console.Write("Enter the position at which to insert:");
+                        k = Convert.ToInt32(Console.ReadLine());
+                        list.InsertANodeAtGivenPosition(data, k);
+                        break;
+                    case 9:
+                        list.DeleteFirstNode();
+                        break;
+                    case 10:
+                        list.DeleteEndNode();
+                        break;
+                    case 11:
+                        Console.Write("Enter the element to be deleted:");
+                        data = Convert.ToInt32(Console.ReadLine());
+                        list.DeleteNode(data);
+                        break;
+                    case 12:
+                        list.ReverseList();
+                        break;
+                    case 13:
+                        list.BubbleSortExcByData();
+                        break;
+                    case 14:
+                        list.BubbleSortExcByLinks();
+                        break;
+                    default:
+                        Console.WriteLine("Wrong Choice");
+                        break;
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("Exiting");
+        }
+    }
+}
---
+++ SingleLinkedListProject/SingleLinkedList.cs
@@ -0,0 +1,358 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace SingleLinkedListProject
+{
+    class SingleLinkedList
+    {
+        private Node start;
+
+        public SingleLinkedList()
+        {
+            start = null;
+        }
+
+        public void DisplayList()
+        {
+            Node p;
+            if(start==null)
+            {
+                Console.WriteLine("List is empty");
+                return;
+            }
+            Console.Write("List is : ");
+            p = start;
+            while(p!=null)
+            {
+                Console.Write(p.info + " ");
+                p = p.link;
+            }
+            Console.WriteLine();
+        }
+
+        public void CountNodes()
+        {
+            Node p = start;
+            int n = 0;
+            while(p!=null)
+            {
+                n++;
+                p = p.link;
+            }
+            Console.WriteLine("Number of Nodes is" + n);
+        }
+
+        public void Search(int x)
+        {
+            Node p = start;
+            int position=0;
+            while(p!=null)
+            {
+                if (p.info == x)
+                    break;
+                position++;
+                p = p.link;   
+            }
+            if (p == null)
+                Console.WriteLine(+x+ "not found in the List");
+            else
+                Console.WriteLine(+x+ "found in position" + position);
+        }
+
+        public void ReferenceToLastNode()
+        {
+            Node p = start;
+            while(p.link!=null)
+            {
+                p = p.link;
+            }
+            Console.WriteLine("Reference to Last Node is:" + p);
+        }
+
+        public void ReferenceToSecondLastNode()
+        {
+            Node p = start;
+            while(p.link.link!=null)
+            {
+                p = p.link;
+            }
+            Console.WriteLine("Reference to Second Last Node is:" + p);
+        }
+
+        public void ReferenceToNodeWithParticularInfo(int x)
+        {
+            Node p = start;
+            while(p!=null)
+            {
+                if (p.info == x)
+                    break;
+                p = p.link;
+            }
+            Console.WriteLine("Reference to Node with Particular Info " + x + " is in:"+p);
+        }
+
+        public void ReferenceToPredecessorOfNodeWithParticularInfo(int x)
+        {
+            Node p = start;
+            while(p!=null)
+            {
+                if (p.link.info == x)
+                    break;
+                p = p.link;
+            }
+            Console.WriteLine("Reference To Predecessor Of Node With Particular Info " + x + " is in:" + p);
+        }
+
+        //public void ReferenceToNodeAtParticularPosition()
+        //{
+        //    Node p = start;
+        //    int i,k;
+        //    if (i = 1; i < k && p != null; i++)
+        //        {
+        //        p = p.link;
+        //    }
+        //}
+
+        public void InsertInBeginning(int data)
+        {
+            Node temp = new Node(data);
+            temp.link = start;
+            start = temp;
+        }
+
+        public void InsertInEnd(int data)
+        {
+            Node p;
+            Node temp = new Node(data);
+            
+            if(start==null)
+            {
+                start = temp;
+                return;
+            }
+            p = start;
+            while(p.link!=null)
+            {
+                p = p.link;
+                temp = p.link;
+            }
+        }
+
+        public void CreateList()
+        {
+            int i, n, data;
+            Console.WriteLine("Enter the number of nodes:");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            if (n == 0)
+                return;
+            for(i=0;i<n;i++)
+            {
+                Console.WriteLine("Enter the element to be inserted:");
+                data = Convert.ToInt32(Console.ReadLine());
+                InsertInEnd(data);
+            }
+        }
+
+        public void InsertANodeAfterGivenNode(int data,int x)
+        {
+            Node p = start;
+            while(p!=null)
+            {
+                if (p.info == x)
+                    break;
+                p = p.link;
+            }
+            if (p == null)
+                Console.WriteLine(x + " is not present in the list");
+            else
+            {
+                Node temp = new Node(data);
+                temp.link = p.link;
+                p.link = temp;
+            }
+        }
+
+        public void InsertANodeBeforeGivenNode(int data, int x)
+        {
+            Node temp;
+            /*If list is empty*/
+            if (start == null)
+            {
+                Console.WriteLine("List is empty");
+                return;
+            }
+
+            /*If x is present in first node,new node is to be inserted before first node*/
+            if(x==start.info)
+            {
+                temp = new Node(data);
+                temp.link = start;
+                start = temp;
+                return;
+            }
+            
+            /*Find reference to predecessor of node containing x*/
+            Node p = start;
+            while(p.link!=null)
+            {
+                if (p.link.info == x)
+                    break;
+                p = p.link;
+            }
+            if(p.link==null)
+                Console.WriteLine(x + " is not present in the list");
+            else
+            {
+                temp = new Node(data);
+                temp.link = p.link;
+                p.link = temp;
+            }
+        }
+
+        public void InsertANodeAtGivenPosition(int data,int k)
+        {
+            Node temp;
+            int i;
+
+            /*Insert in first position*/
+            if(k==1)
+            {
+                temp = new Node(data);
+                temp.link = start;
+                start = temp;
+                return;
+            }
+
+            Node p = start;
+            for(i=1;i<k-1 && p!=null;i++)
+            {
+                p = p.link;
+            }
+            if (p == null)
+                Console.WriteLine("You can insert only upto " + i + "th position");
+            else
+            {
+                temp = new Node(data);
+                temp.link = p.link;
+                p.link = temp;
+            }
+        }
+
+        public void DeleteFirstNode()
+        {
+            if (start == null)
+                return;
+            start = start.link;
+        }
+
+        public void DeletionOfOnlyNode()
+        {
+            start = null;
+        }
+
+        public void DeleteEndNode()
+        {
+            if (start == null)
+                return;
+            if (start.link == null)
+            {
+                start = null;
+                return;
+            }
+            Node p = start;
+            while (p.link.link!= null)
+            {
+                p = p.link;
+            }
+            p.link = null;
+        }
+
+        public void DeleteNode(int x)
+        {
+            if (start == null)
+            {
+                Console.WriteLine("List is Empty");
+                return;
+            }
+
+            if(start.info==x)
+            {
+                start = start.link;
+                return;
+            }
+
+            /*Deletion in between or at the end*/
+
+            Node p = start;
+            while(p.link!=null)
+            {
+                if (p.link.info == x)
+                    break;
+                p = p.link;
+            }
+            if (p.link == null)
+                Console.WriteLine("Element " + x + " not found in the list");
+            else
+                p.link = p.link.link;
+        }
+
+        public void ReverseList()
+        {
+            Node prev, p, next;
+            prev = null;
+            p = start;
+            while(p!=null)
+            {
+                next = p.link;
+                p.link = prev;
+                prev = p;
+                p = next;
+            }
+        }
+
+        public void BubbleSortExcByData()
+        {
+            Node end, p, q;
+            for(end=null;end!=start.link;end=p)
+            {
+                for(p=start;p.link!=end;p=p.link)
+                {
+                    q = p.link;
+                    if(p.info>q.info)
+                    {
+                        int temp = p.info;
+                        p.info = q.info;
+                        q.info = temp;
+                    }
+                }
+            }
+        }
+
+        public void BubbleSortExcByLinks()
+        {
+            Node end, p, q, r, temp;
+            for (end = null; end != start.link; end = p)
+            {
+                for (r = p = start; p.link != end; r = p, p= p.link)
+                {
+                    q = p.link;
+                    if (p.info > q.info)
+                    {
+                        p.link = q.link;
+                        q.link = p;
+                        if (p != start)
+                            r.link = q;
+                        else
+                            start = q;
+                        temp = p;
+                        p = q;
+                        q = temp;
+                    }
+                }
+            }
+        }
+    }
+}
